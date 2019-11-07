9cba90d9d3cc4ba2efd5902d14c030e01aba9555
Protesticon <kangyi@pku.edu.cn>
2019-11-06 16:12:31 -0800

    model decided

--- SkeNet_Illustris3_Ver1.0/model.py
+++ SkeNet_Illustris3_Ver1.0/model.py
@@ -120,7 +120,7 @@ class AverageMeter(object):
         self.val = val
         self.sum += val * n
         self.count += n
-        self.avg = self.sum / self.count
+        self.avg = self.sum / self.count 
 
 
 '''
